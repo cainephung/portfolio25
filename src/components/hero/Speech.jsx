923df7678f6723d84ce68a847bcc6fb2e1aa585a
Caine Phung <hphung3324@conestogac.on.ca>
2025-07-28 22:04:44 -0400

    init

---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,32 @@
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "motion/react";
+
+const Speech = () => {
+  return (
+    <motion.div
+      className="bubbleContainer"
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+    >
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Let’s turn ideas into reality!",
+            1000,
+            "Always learning! Always building!",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={60}
+          // omitDeletionAnimation
+          repeat={Infinity}
+        />
+      </div>
+      {/* <img src="/man.png" alt="" /> */}
+    </motion.div>
+  );
+};
+
+export default Speech;
