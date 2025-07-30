803932bc40d09005ff13693e750e70b565a91dd3
Caine Phung <hphung3324@conestogac.on.ca>
2025-07-30 01:11:06 -0400

    trying to make it more reponsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-// import Hero from "./components/hero/Hero";
-// import Services from "./components/services/Services";
-// import Portfolio from "./components/portfolio/Portfolio";
-// import Contact from "./components/contact/Contact";
-
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import LazyLoad from "react-lazyload";
-// import Experience from "./components/hero/Experience";
 
 const Hero = lazy(() => import("./components/hero/Hero"));
 const Services = lazy(() => import("./components/services/Services"));
@@ -14,6 +8,10 @@ const Contact = lazy(() => import("./components/contact/Contact"));
 const Experience = lazy(() => import("./components/experience/Experience"));
 
 const App = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="container">
       <Suspense fallback={"loading..."}>
@@ -32,15 +30,17 @@ const App = () => {
       </Suspense>
       <Suspense fallback={"loading..."}>
         <LazyLoad height={"100vh"} offset={-100}>
-          <section id="#home">
+          <section id="#experience">
             <Experience />
           </section>
         </LazyLoad>
       </Suspense>
       <Suspense fallback={"loading..."}>
-        <LazyLoad height={"600vh"} offset={-100}>
+        <LazyLoad height={"100vh"} offset={-100}>
           {/* <section id="#portfolio"> */}
-          <Portfolio />
+          <section id="portfolio">
+            <Portfolio />
+          </section>{" "}
           {/* </section> */}{" "}
         </LazyLoad>
       </Suspense>
