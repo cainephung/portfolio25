89f0b7b757f343873b81db8c3783837eb0919de8
Caine Phung <hphung3324@conestogac.on.ca>
2025-08-10 17:25:38 -0400

    -> about

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
           </li>
           <li>
             <a href="#services" onClick={handleClose}>
-              Services
+              About
             </a>
           </li>
           <li>
